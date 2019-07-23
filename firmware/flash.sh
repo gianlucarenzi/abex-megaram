497b1ee5e22fc132ed736c135c327ca7ca94d577
Gianluca Renzi <gianlucarenzi@eurek.it>
2019-07-23 12:17:56 +0200

    Removed

--- firmware/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; flash write_image erase unlock build/megaram.elf; reset run; shutdown"
--- firmware/halt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; shutdown"
