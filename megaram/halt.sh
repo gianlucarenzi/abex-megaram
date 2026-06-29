5d4b9952d9eb4531944cde9740c3092313febc0f
gianlucarenzi <gianlucarenzi@eurek.it>
2026-06-29 17:07:25 +0200

    chore(openocd): migra script e Makefile a OpenOCD STM fork con supporto STM32H5
    - Makefile.common: aggiunte variabili ST_OCD, ST_SCRIPTS, OCD_CFG, OCD_FLAGS
      che puntano all'installazione STMicroelectronics in $HOME
    - Makefile: aggiunti target flash, halt, reset che usano le variabili
    - flash.sh / halt.sh / reset.sh: aggiornata config da stm32f4eval.cfg a
      stm32h5e5zj.cfg; aggiunto flag -s per il search path degli script TCL

--- megaram/halt.sh
+++ megaram/halt.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-openocd -f openocd/stm32f4eval.cfg \
--c "init; targets; reset init; wait_halt; poll; shutdown"
+ST_OCD="${HOME}/bin/openocd"
+ST_SCRIPTS="${HOME}/share/openocd/scripts"
+OCD_CFG="openocd/stm32h5e5zj.cfg"
+
+"${ST_OCD}" -s "${ST_SCRIPTS}" -f "${OCD_CFG}" \
+    -c "init; targets; reset init; wait_halt; poll; shutdown"
