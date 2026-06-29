640f66c169305dce92e4fba43c4a1a0098132bd5
gianlucarenzi <gianlucarenzi@eurek.it>
2026-06-29 09:04:32 +0200

    Revert CPU clock to 180MHz: STM32F429 datasheet max is 180MHz
    216MHz with FLASH_LATENCY_7 was out-of-spec for STM32F429IGTx (DS8080).
    Restore PLL multiplier to 180, LATENCY_5 (correct for 180MHz at 3.3V),
    and all associated tick/sysclk values.

--- megaram/src/main.c
+++ megaram/src/main.c
@@ -99,11 +99,11 @@ static void usart_config(int baudrate);
   */
 void SystemClock_Config(void)
 {
-	LL_FLASH_SetLatency(LL_FLASH_LATENCY_7);
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
 
-	if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_7)
+	if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5)
 	{
-		Error_Handler("SystemClock_Config");  
+		Error_Handler("SystemClock_Config");
 	}
 	LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
 	LL_PWR_EnableOverDriveMode();
@@ -114,7 +114,7 @@ void SystemClock_Config(void)
 	while(LL_RCC_HSE_IsReady() != 1)
 		;;
 
-	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 216, LL_RCC_PLLP_DIV_2);
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 180, LL_RCC_PLLP_DIV_2);
 	LL_RCC_PLL_Enable();
 
 	/* Wait till PLL is ready */
@@ -130,10 +130,10 @@ void SystemClock_Config(void)
 	while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
 		;;
 
-	/* We are running @ 216Mhz */
-	LL_Init1msTick(216000000);
+	/* We are running @ 180Mhz */
+	LL_Init1msTick(180000000);
 	LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
-	LL_SetSystemCoreClock(216000000);
+	LL_SetSystemCoreClock(180000000);
 	LL_RCC_SetTIMPrescaler(LL_RCC_TIM_PRESCALER_TWICE);
 }
 
